0ea4dbe6d7a873ef96f1c98c886fcc684e59dae1
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2020-05-27 12:00:46 -0400

    Cambios en base de datos.

---
+++ RegistroPrestamo/Migrations/20200527155633_«InitMigration».cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RegistroPrestamo.Migrations
+{
+    public partial class InitMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Personas",
+                columns: table => new
+                {
+                    personaId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    nombre = table.Column<string>(maxLength: 50, nullable: false),
+                    telefono = table.Column<string>(maxLength: 10, nullable: false),
+                    cedula = table.Column<string>(nullable: false),
+                    direccion = table.Column<string>(maxLength: 40, nullable: false),
+                    fecha = table.Column<DateTime>(nullable: false),
+                    balance = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personas", x => x.personaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prestamos",
+                columns: table => new
+                {
+                    prestamoId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    fecha = table.Column<DateTime>(nullable: false),
+                    personaId = table.Column<int>(nullable: false),
+                    concepto = table.Column<string>(maxLength: 40, nullable: false),
+                    monto = table.Column<decimal>(nullable: false),
+                    balance = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prestamos", x => x.prestamoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Personas");
+
+            migrationBuilder.DropTable(
+                name: "Prestamos");
+        }
+    }
+}
