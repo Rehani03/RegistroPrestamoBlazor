4cd09acf168457d442fea263a34f174529b145de
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2020-05-28 18:05:35 -0400

    Ultimos cmabios..

---
+++ RegistroPrestamoTests2/BLL/PersonasBLLTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegistroPrestamo.BLL;
+using RegistroPrestamo.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegistroPrestamo.BLL.Tests
+{
+    [TestClass()]
+    public class PersonasBLLTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Personas p = new Personas();
+            bool paso = false;
+            p.personaId = 0;
+            p.nombre = "Jose Perez";
+            p.fecha = DateTime.Now;
+            p.cedula = "40225550022";
+            p.direccion = "Calle Duarte #44";
+            p.telefono = "8099637885";
+            p.balance = 0;
+            paso = PersonasBLL.Guardar(p);
+            Assert.AreEqual(paso, true);
+
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Personas personas;
+            personas = PersonasBLL.Buscar(2);
+            Assert.AreEqual(personas, personas);
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            var lista = new List<Personas>();
+            lista = PersonasBLL.GetList(p => true);
+            Assert.AreEqual(lista, lista);
+        }
+
+        [TestMethod()]
+        public void ExisteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            bool paso;
+            paso = PersonasBLL.Eliminar(1);
+            Assert.AreEqual(paso, true);
+        }
+    }
+}
---
+++ RegistroPrestamoTests2/BLL/PrestamosBLLTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegistroPrestamo.BLL;
+using RegistroPrestamo.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegistroPrestamo.BLL.Tests
+{
+    [TestClass()]
+    public class PrestamosBLLTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Prestamo prestamo = new Prestamo();
+            prestamo.prestamoId = 0;
+            prestamo.fecha = DateTime.Now;
+            prestamo.personaId = 1;
+            prestamo.concepto = "Comida";
+            prestamo.monto = 100;
+            prestamo.balance = 100;
+            bool paso = PrestamosBLL.Guardar(prestamo);
+            Assert.AreEqual(paso, true);
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            bool paso;
+            paso = PrestamosBLL.Eliminar(3);
+            Assert.AreEqual(paso, true);
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Prestamo prestamo;
+            prestamo = PrestamosBLL.Buscar(2);
+            Assert.AreEqual(prestamo, prestamo);
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            var lista = new List<Prestamo>();
+            lista = PrestamosBLL.GetList(p => true);
+            Assert.AreEqual(lista, lista);
+        }
+    }
+}
