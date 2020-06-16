ef1b996a1a378d69329fed38695d9a039c405492
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2020-06-16 16:51:36 -0400

    Moviendo BD

--- RegistroPrestamo/DAL/Contexto.cs
+++ RegistroPrestamo/DAL/Contexto.cs
@@ -14,7 +14,7 @@ namespace RegistroPrestamo.DAL
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite(@"Data Source=c:\Prestamos_db\PrestamosDB.db");
+            optionsBuilder.UseSqlite(@"Data Source=DATA\PrestamosDB.db");
         }
 
     }
